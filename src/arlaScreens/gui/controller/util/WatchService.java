26a9185cd59b072c4c4c1d2d4aea6e090ad7ac2c
Mathias Mols Thiim <math75n9@easv.dk>
2021-06-01 10:42:32 +0200

    Update WatchService.java
    JavaDoc

--- src/arlaScreens/gui/controller/util/WatchService.java
+++ src/arlaScreens/gui/controller/util/WatchService.java
@@ -6,7 +6,11 @@ import java.nio.file.*;
 public class WatchService extends Thread {
 
 
-
+    /**
+     * Metode der Overrider run metoden klassen arver fra Thread.
+     * Metoden opretter en ny Watch Service, opretter en path, og registrerer en watckey til den oprettede path.
+     * Derefter køres et uendeligt loop så metoden konstant holder øje med ændringer i det registrerede path.
+     */
     public void run() {
         try {
              java.nio.file.WatchService watchService = FileSystems.getDefault().newWatchService();
